ab45577e84bf0fa59afc0fd3335f38721e91f014
Noah Lopez <lopeznoah568@gmail.com>
2020-10-28 08:50:40 -0500

    Comments

---
+++ Comments/commentsOutline.py
@@ -0,0 +1,23 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a single line comment.
+
+#school is annoying
+
+#2) Make a multi-line comment.
+
+'''
+hi
+no
+bye
+noah
+'''
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+
+var4 = 4 #single line comment
+print(var4)
