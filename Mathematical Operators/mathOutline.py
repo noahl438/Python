95b5b0c1ffac6ef87ed8b1772b412f035e13ac36
Noah Lopez <lopeznoah568@gmail.com>
2020-11-19 22:11:57 -0600

    Update mathOutline.py

--- Mathematical Operators/mathOutline.py
+++ Mathematical Operators/mathOutline.py
@@ -6,57 +6,58 @@ Fill in this outline as the instructor goes through the lesson.
 #1) Make two string variables and use the + operator to combine them into
 #one new variable.
 
-
+x = "your" + " name"
 
 #2) Make two int variables and use the + operator to combine them into
 #one new variable.
 
-
+x = 4 + 4
 
 #3) Make two int variables and use the - operator to combine them into
 #one new variable.
 
-
+y = 4 - 3
 
 #4) Make two float variables and use the - operator to combine them into
 #one new variable.
 
+z = 1.23 - 4.56
 
 #5) Make two int variables and use the * operator to combine them into
 #one new variable.
 
-
+a = 3 * 19
 
 #6) Make one string variable and one int variable and use the * operator 
 #to combine them into one new variable.
 
-
+b = "noah " * 3
 
 #7) Make two int variables and use the / operator to combine them into
 #one new variable.
 
-
+c = 4 / 4
 
 #8) Make two float variables and use the / operator to combine them into
 #one new variable.
 
-
+d = 4.12 / 4.12
 
 #9) Make two int variables and use the % operator to combine them into
 #one new variable.
 
-
+e = 7 % 4
 
 #10) Make two int variables and use the // operator to combine them into
 #one new variable.
 
-
+f = 4 // 4
 
 #11) Make two int variables and use the ** operator to combine them into
 #one new variable.
 
-
+g = 4 ** 5
 
 #12) Make any type of operation with a common SYNTAX error
 
-
+p = "hi"  56
