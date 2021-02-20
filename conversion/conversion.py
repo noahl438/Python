e3d0bb854c58ea4c1a74ca9ea83396dd373e596c
Noah Lopez <lopeznoah568@gmail.com>
2021-02-20 11:36:46 -0600

    project 2

---
+++ conversion/conversion.py
@@ -0,0 +1,32 @@
+'''
+
+
+@author: Noah J Lopez
+'''
+#Use input() to get the number of miles from the user. And store
+#that int in a variable called miles.
+miles = input("Enter an amount of miles you would like converted")
+
+#Convert miles to yards, using the following:
+# 1 mile = 1760 yards.
+#
+#Store the value in a variable called yards and print it out with a 
+#simple statement.
+yards = miles * 1760
+print(str(miles) + " miles converts to " + str(yards) + " yards.")
+
+#Convert miles to feet, using the following:
+# 1 mile = 5280 feet.
+#
+#Store the value in a variable called feet and print it out with a 
+#simple statement.
+feet = miles * 5280
+print(str(miles) + " miles converts to " + str(feet) + " feet.")
+
+#Convert miles to inches, using the following:
+# 1 mile = 63,360 inches.
+#
+#Store the value in a variable called inches and print it out with a 
+#simple statement.
+inches = miles * 63360
+print(str(miles) + " miles converts to " + str(inches) + " inches.")
---
+++ machine/machine.py
@@ -0,0 +1,12 @@
+'''
+Created on Dec 2, 2020
+
+@author: ITAUser
+
+Objective - Make a program that can have a conversation with me
+'''
+print("hello")
+name = input("What is your name?")
+print("Hello", name)
+age = input("How old are you?") 
+grade = input("You're a junior then?")
